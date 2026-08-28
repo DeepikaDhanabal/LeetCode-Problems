a1353dc17e98c94eb84bbd0ed085c0ad5bd2574a
DeepikaDhanabal <deepikad12005@gmail.com>
2026-08-28 18:18:01 +0530

    Time: 17 ms (17.56%), Space: 47.6 MB (48.17%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        int duplicate = 0;
+        int missing = 0;
+
+        for (int num : nums) {
+
+            if (set.contains(num)) {
+                duplicate = num;
+            }
+
+            set.add(num);
+        }
+
+        for (int i = 1; i <= nums.length; i++) {
+
+            if (!set.contains(i)) {
+                missing = i;
+                break;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
