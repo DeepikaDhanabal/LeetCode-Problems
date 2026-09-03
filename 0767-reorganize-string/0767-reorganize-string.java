21df583cc5fadb4972c5974911a0cf9341b53f4a
DeepikaDhanabal <deepikad12005@gmail.com>
2026-09-03 14:19:05 +0530

    Time: 3 ms (76.97%), Space: 43 MB (54.96%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -0,0 +1,56 @@
+class Solution {
+    public String reorganizeString(String s) {
+
+        int[] freq = new int[26];
+
+        // Count frequency
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        // Max heap
+        PriorityQueue<int[]> pq =
+            new PriorityQueue<>((a, b) -> b[1] - a[1]);
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                pq.offer(new int[]{i, freq[i]});
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        while (pq.size() > 1) {
+
+            int[] first = pq.poll();
+            int[] second = pq.poll();
+
+            result.append((char) (first[0] + 'a'));
+            result.append((char) (second[0] + 'a'));
+
+            first[1]--;
+            second[1]--;
+
+            if (first[1] > 0) {
+                pq.offer(first);
+            }
+
+            if (second[1] > 0) {
+                pq.offer(second);
+            }
+        }
+
+        // One character remaining
+        if (!pq.isEmpty()) {
+            int[] last = pq.poll();
+
+            if (last[1] > 1) {
+                return "";
+            }
+
+            result.append((char) (last[0] + 'a'));
+        }
+
+        return result.toString();
+    }
+}
